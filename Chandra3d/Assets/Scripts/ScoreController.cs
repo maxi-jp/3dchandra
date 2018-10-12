e4b68f402b396f729f1f4fc824c674424c9b9dac
DiegoGal <diego.davilaguilar@gmail.com>
2018-10-13 01:35:36 +0200

    [Score] Basic score system, with enemy score included at CLife component. Closes #3

---
+++ Chandra3d/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+// Control score UI components
+public class ScoreController : MonoBehaviour {
+
+    public Text scorePointsTxt;  // score points text at UI
+
+    // sets score at UI
+	public void setScore(int newScore)
+    {
+        scorePointsTxt.text = newScore.ToString();
+    }        
+}
